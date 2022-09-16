fcc01d4a5866f103e9d96b8eaf2043e17be92c18
Mate Mark Csornyei <mate@csornyei.com>
2022-09-16 10:07:05 +0200

    Initial commit

---
+++ lib/supply_chain.ex
@@ -0,0 +1,18 @@
+defmodule SupplyChain do
+  @moduledoc """
+  Documentation for `SupplyChain`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SupplyChain.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
