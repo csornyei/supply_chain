82db8157003d686ab477ffe73a556fdaa1a2b5c4
Mate Mark Csornyei <mate@csornyei.com>
2022-12-06 12:17:22 +0100

    updated public api

--- .iex.exs
+++ .iex.exs
@@ -1,13 +1,12 @@
-alias SupplyChain.Boundary.GameSession
 factory_list = [{1,2}, {3,4}, {5,6}]
 :observer.start
 
 
 test_fn = fn ->
-  {:ok, name} = GameSession.new_game(factory: factory_list)
-  GameSession.join(name, "1")
-  GameSession.join(name, "2")
-  GameSession.next_round(name)
-  GameSession.send_buy_message(name, "2", "factory", 1)
+  {:ok, name, _} = SupplyChain.start_game(factory: factory_list)
+  SupplyChain.join(name, "1")
+  SupplyChain.join(name, "2")
+  SupplyChain.next_round(name)
+  SupplyChain.send_buy_message(name, "2", "factory", 1)
   name
 end
--- lib/supply_chain.ex
+++ lib/supply_chain.ex
@@ -7,6 +7,38 @@ defmodule SupplyChain do
     {:ok, id, pid}
   end
 
+  def join(id, player_name) do
+    GameSession.join(id, player_name)
+  end
+
+  def change_role(id, player_name, role_name) do
+    GameSession.change_role(id, player_name, role_name)
+  end
+
+  def get_offers_for_role(id, role_name) do
+    GameSession.get_offers_for_role(id, role_name)
+  end
+
+  def get_player_state(id, player_name) do
+    GameSession.get_player_state(id, player_name)
+  end
+
+  def get_messages(id) do
+    GameSession.get_messages(id)
+  end
+
+  def next_round(id) do
+    GameSession.next_round(id)
+  end
+
+  def send_offer_message(id, seller, amount, price) do
+    GameSession.send_offer_message(id, seller, amount, price)
+  end
+
+  def send_buy_message(id, buyer, seller, amount) do
+    GameSession.send_buy_message(id, buyer, seller, amount)
+  end
+
   defp random_id do
     :crypto.strong_rand_bytes(6) |> Base.url_encode64(padding: false)
   end
